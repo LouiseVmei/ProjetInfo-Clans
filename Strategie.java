ec75073f403b9b05101a6704d5fa205bfad60c96
johanneclc <62894386+johanneclc@users.noreply.github.com>
2020-04-20 09:55:37 +0200

    Update Strategie.java

--- Strategie.java
+++ Strategie.java
@@ -12,33 +12,36 @@ public interface Strategie {
      * @param _colorScore
      * @param _myScore
      * @param _opponentScore
-     * @param _opponentMov movement (source, destination) au tour précédent.
-     * @param _opponentVillages la liste ordonée des villages construits au tour précédent.
+     * @param _opponentMov movement (source, destination) au tour prÃ©cÃ©dent.
+     * @param _opponentVillages la liste ordonÃ©e des villages construits au tour prÃ©cÃ©dent.
      * @return un tableau de 2 entiers, le 1er sera la source de votre coup, et le second sera la destination
      */
     public int[] mouvement( Terrain [] _plateau, int _myColor, int [] _colorScore, int _myScore, int _opponentScore, int [] _opponentMov, int [] _opponentVillages );
     
     /**
      * @param _villages
-     * @return un tableau correspondant à l'ordre dans laquelle les villages seront créés (si nécessaire)
+     * @return un tableau correspondant Ã  l'ordre dans laquelle les villages seront crÃ©Ã©s (si nÃ©cessaire)
      */
     public int[] ordre( int[] _villages );
     
     /**
      * IMPORTANT!!!
-     * Remplissez correctement cette méthode avec le bon format expliqué ci-dessous (-2 points si consigne non respectée)
-     * @return le nom des élèves (sous le format NOM1 NOM2, ex: "FLEURY MICHON") 
+     * Remplissez correctement cette mÃ©thode avec le bon format expliquÃ© ci-dessous (-2 points si consigne non respectÃ©e)
+     * @return le nom des Ã©lÃ¨ves (sous le format NOM1 NOM2, ex: "FLEURY MICHON") 
      * NOM1 et NOM2 sont uniquement les noms de famille de votre binome
-     * (si nom de famille identique à un autre élève, vous pouvez ajouter l'initiale de votre prénom, ex: A.FLEURY MICHON
+     * (si nom de famille identique Ã  un autre Ã©lÃ¨ve, vous pouvez ajouter l'initiale de votre prÃ©nom, ex: A.FLEURY MICHON
      */
-    public String getName();
+    public String getName(){
+        return "MORIN PARIS VILLETTE CALCOEN";
+    }
 
     /**
      * IMPORTANT!!!
-     * Remplissez correctement cette méthode avec le bon format expliqué ci-dessous (-2 points si consigne non respectée)
+     * Remplissez correctement cette mÃ©thode avec le bon format expliquÃ© ci-dessous (-2 points si consigne non respectÃ©e)
      * @return le groupe de TD/TP (1, 2, 3, 4, 5, 6 ou P pour les enseignants)
      * Uniquement une lettre ou un chiffre (ex: "1" et surtout pas "groupe 1")
      */    
-    public String getGroupe();
-    
+    public String getGroupe(){
+        return "1";
+    }
 }
